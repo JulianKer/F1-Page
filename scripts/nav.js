4a2993ef9f4b7d86726991f70cf031b851e54b92
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-06-24 20:54:10 -0300

    52 - Hice lo de que cuando estas en mobile, al desplegar el menu y toquar en otro lado de la pantalla, se oculta.

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,9 +3,12 @@ var menu_desplegable_oculto = document.querySelector(".menu_desplegable_oculto")
 
 let misEquipos = document.querySelectorAll(".equipo");
 
+// hago un if  porq como el index esta afuera de las carpetas, tengo q usar el ../ en algunas rutas y si NO
+// estoy en el home, SI que la tengo q usar
 if (document.title === "Inicio | F1") {
     console.log("INICIO")
 
+    // cuando toco el menu, despliego el menu, y si ya esta desplegado, lo oculto
     img_menu.addEventListener("click", ()=>{
         if(img_menu.src.includes("assets/icons/burger_menu.svg")){
             img_menu.src = "assets/icons/cruz.svg";
@@ -16,16 +19,28 @@ if (document.title === "Inicio | F1") {
         }
     });
     
+    // tambien, si se reajusta la pantalla a +915px, es pq ya no necesito el menu responsive y muestra el menu original, pero esto
+    // se hace para que SI el menu esta desplegado, y se agranda la pantalla, que se oculte el menu
     window.addEventListener("resize", ()=>{
         if(window.innerWidth > 915){
             menu_desplegable_oculto.style = "display: none;";
             img_menu.src = "assets/icons/burger_menu.svg";
         }
     })
+
+    // este lo hago para que cuando el menu esta desplegado, si toco en cualquier lado de la pantalla que NO sea ni el menu desplegable ni 
+    // la imagen del menu, este menu se oculte
+    document.addEventListener("click", (e)=>{
+        if (!menu_desplegable_oculto.contains(e.target) && !img_menu.contains(e.target)) {
+            img_menu.src = "assets/icons/burger_menu.svg";
+            menu_desplegable_oculto.style = "display: none";
+        }
+    });
     
 }else{
     console.log("OTRAS PAGINAS")
 
+    // cuando toco el menu, despliego el menu, y si ya esta desplegado, lo oculto
     img_menu.addEventListener("click", ()=>{
         if(img_menu.src.includes("assets/icons/burger_menu.svg")){
             img_menu.src = "../assets/icons/cruz.svg";
@@ -36,15 +51,28 @@ if (document.title === "Inicio | F1") {
         }
     });
     
+     // tambien, si se reajusta la pantalla a +915px, es pq ya no necesito el menu responsive y muestra el menu original, pero esto
+    // se hace para que SI el menu esta desplegado, y se agranda la pantalla, que se oculte el menu
     window.addEventListener("resize", ()=>{
         if(window.innerWidth > 915){
             menu_desplegable_oculto.style = "display: none;";
             img_menu.src = "../assets/icons/burger_menu.svg";
         }
     })
+
+    // este lo hago para que cuando el menu esta desplegado, si toco en cualquier lado de la pantalla que NO sea ni el menu desplegable ni 
+    // la imagen del menu, este menu se oculte
+    document.addEventListener("click", (e)=>{
+        if (!menu_desplegable_oculto.contains(e.target) && !img_menu.contains(e.target)) {
+            img_menu.src = "../assets/icons/burger_menu.svg";
+            menu_desplegable_oculto.style = "display: none";
+        }
+    });
 }
 
 
+
+// este es para que cuando toque algun equipo del desplegable de 'equipos', te redireccione al equipo correspondiente
 misEquipos.forEach((equipo)=>{
     equipo.addEventListener("click", (e)=>{
         e.preventDefault();
