a981b1dca06060062a9e92841ad5c0f46293f81c
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-07-13 16:17:30 -0300

    80 - Le puse el borde cuando haces hover a cada equipo con su color correspondiente.

--- scripts/equipos.js
+++ scripts/equipos.js
@@ -21,6 +21,7 @@ let arrayEquipos = [
             "img":'../assets/img/img_pilotos/per.png',
         }],
         "auto":'../assets/img/cars_equipos/red-bull-racing.avif',
+        "bordeEquipo":'borde_redbull_piloto',
     },
     {
         "id":'ferrari',
@@ -40,6 +41,7 @@ let arrayEquipos = [
             "img":'../assets/img/img_pilotos/sai.png',
         }],
         "auto":'../assets/img/cars_equipos/ferrari.avif',
+        "bordeEquipo":'borde_ferrari_piloto',
     },
     {
         "id":'mclaren',
@@ -59,6 +61,7 @@ let arrayEquipos = [
             "img":'../assets/img/img_pilotos/pia.png',
         }],
         "auto":'../assets/img/cars_equipos/mclaren.avif',
+        "bordeEquipo":'borde_mclaren_piloto',
     },
     {
         "id":'mercedes',
@@ -78,6 +81,7 @@ let arrayEquipos = [
             "img":'../assets/img/img_pilotos/rus.png',
         }],
         "auto":'../assets/img/cars_equipos/mercedes.avif',
+        "bordeEquipo":'borde_mercedes_piloto',
     },
     {
         "id":'astonMartin',
@@ -97,6 +101,7 @@ let arrayEquipos = [
             "img":'../assets/img/img_pilotos/str.png',
         }],
         "auto":'../assets/img/cars_equipos/aston-martin.avif',
+        "bordeEquipo":'borde_aston_martin_piloto',
     },
     {
         "id":'rb',
@@ -116,6 +121,7 @@ let arrayEquipos = [
             "img":'../assets/img/img_pilotos/ric.png',
         }],
         "auto":'../assets/img/cars_equipos/rb.avif',
+        "bordeEquipo":'borde_rb_piloto',
     },
     {
         "id":'haas',
@@ -135,6 +141,7 @@ let arrayEquipos = [
             "img":'../assets/img/img_pilotos/mag.png',
         }],
         "auto":'../assets/img/cars_equipos/haas.avif',
+        "bordeEquipo":'borde_haas_piloto',
     },
     {
         "id":'alpine',
@@ -154,6 +161,7 @@ let arrayEquipos = [
             "img":'../assets/img/img_pilotos/oco.png',
         }],
         "auto":'../assets/img/cars_equipos/alpine.avif',
+        "bordeEquipo":'borde_alpine_piloto',
     },
     {
         "id":'williams',
@@ -173,6 +181,7 @@ let arrayEquipos = [
             "img":'../assets/img/img_pilotos/sar.png',
         }],
         "auto":'../assets/img/cars_equipos/williams.avif',
+        "bordeEquipo":'borde_williams_piloto',
     },
     {
         "id":'kickSauber',
@@ -192,6 +201,7 @@ let arrayEquipos = [
             "img":'../assets/img/img_pilotos/zho.png',
         }],
         "auto":'../assets/img/cars_equipos/kick-sauber.avif',
+        "bordeEquipo":'borde_kick_sauber_piloto',
     },
 ]
 
@@ -205,7 +215,7 @@ arrayEquipos.forEach((equipo, index)=>{
     let posicion = index+1;
 
     eContenedorEquipos.innerHTML += `
-            <a href="equipoEspecifico.html" class="e_equipo" id="${equipo.id}">
+            <a href="equipoEspecifico.html" class="e_equipo ${equipo.bordeEquipo}" id="${equipo.id}">
                 <section class="reng_1 fuente_f1_bold">
                     <p>${posicion}</p>
                     <div>
